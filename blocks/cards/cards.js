1999fe7be263cfafdfb495c81952a3cd01d64e04
Varun Nagpal <vnagpal@adobe.com>
2023-04-06 11:41:21 +0530

    changes for PoC

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -4,12 +4,26 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+	  console.log(row.innerHTML);
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+	let found = false;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture') && !found) {
+		  div.className = 'cards-card-image';
+		  div.querySelectorAll('img').style = 'border-radius:25px';
+		  found = true;
+	  }
+      else {
+		  div.className = 'cards-card-body';
+		  div.querySelectorAll('img').forEach((img) => {
+				img.width=24;
+				img.height=24;
+		  });
+	  }
     });
+	li.children[4].classList.add("right");
+	li.children[5].classList.add("right");
     ul.append(li);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
